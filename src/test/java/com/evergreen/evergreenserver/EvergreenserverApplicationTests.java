6e18cfdc75498b88aa822809af003a48b9c157e8
k-jaehyun <wogus146@gmail.com>
2024-04-06 20:36:00 +0900

    init

---
+++ src/test/java/com/evergreen/evergreenserver/EvergreenserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evergreen.evergreenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvergreenserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
