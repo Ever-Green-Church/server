243d75edc1c957b866a05c736a47c6eb6d745923
k-jaehyun <wogus146@gmail.com>
2024-04-09 19:49:34 +0900

    User entity

---
+++ src/main/java/com/evergreen/evergreenserver/domain/user/entity/User.java
@@ -0,0 +1,19 @@
+package com.evergreen.evergreenserver.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+
+}
