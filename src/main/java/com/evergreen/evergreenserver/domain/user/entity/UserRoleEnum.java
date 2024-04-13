d63c7e1e1c5d3f932cdbd5968043e432a390a51c
k-jaehyun <wogus146@gmail.com>
2024-04-13 22:39:04 +0900

    UserRoleEnum

---
+++ src/main/java/com/evergreen/evergreenserver/domain/user/entity/UserRoleEnum.java
@@ -0,0 +1,29 @@
+package com.evergreen.evergreenserver.domain.user.entity;
+
+public enum UserRoleEnum {
+
+  //유저 권한
+  USER(Authority.USER),
+  ADMIN(Authority.ADMIN),
+  BANNED(Authority.BANNED);
+
+  private final String authority;
+
+  //유저 권한 생성자
+  UserRoleEnum(String authority) {
+    this.authority = authority;
+  }
+
+  //유저 권한 조회
+  public String getAuthority() {
+    return this.authority;
+  }
+
+  //유저 권한
+  public static class Authority {
+
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String BANNED = "ROLE_BANNED";
+  }
+}
