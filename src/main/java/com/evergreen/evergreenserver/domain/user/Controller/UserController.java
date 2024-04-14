fc48febefe720cbc1e81f07fd5baf79e3ad2b048
k-jaehyun <wogus146@gmail.com>
2024-04-14 23:05:14 +0900

    UserController

---
+++ src/main/java/com/evergreen/evergreenserver/domain/user/Controller/UserController.java
@@ -0,0 +1,11 @@
+package com.evergreen.evergreenserver.domain.user.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+  //카카오로그인
+}
