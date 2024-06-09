c7ae3abaeb847d19f57ab457a652fa3b41bfd723
k-jaehyun <wogus146@gmail.com>
2024-06-10 00:23:02 +0900

    fix: PrayTitleRepository, Fetch Join 적용

--- src/main/java/com/evergreen/evergreenserver/domain/praytitle/repository/PrayTitleRepository.java
+++ src/main/java/com/evergreen/evergreenserver/domain/praytitle/repository/PrayTitleRepository.java
@@ -4,12 +4,11 @@ import com.evergreen.evergreenserver.domain.praytitle.entity.PrayTitle;
 import com.evergreen.evergreenserver.domain.user.entity.User;
 import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PrayTitleRepository extends JpaRepository<PrayTitle, Long> {
 
-  @EntityGraph(attributePaths = {"user"})
-  Optional<PrayTitle> findByUser(@Param("user") User user);
+  @Query("SELECT prayTitle FROM PrayTitle prayTitle JOIN FETCH prayTitle.user WHERE prayTitle.user = :user")
+  Optional<PrayTitle> findByUserWithFetchJoin(@Param("user") User user);
 }
