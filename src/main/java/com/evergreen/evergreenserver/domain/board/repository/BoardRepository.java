bc3cc505dc9634e74533cb6f30fa7d408859573a
k-jaehyun <wogus146@gmail.com>
2024-06-12 00:03:41 +0900

    add: BoardRepository, @Repository 추가

--- src/main/java/com/evergreen/evergreenserver/domain/board/repository/BoardRepository.java
+++ src/main/java/com/evergreen/evergreenserver/domain/board/repository/BoardRepository.java
@@ -2,7 +2,9 @@ package com.evergreen.evergreenserver.domain.board.repository;
 
 import com.evergreen.evergreenserver.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
