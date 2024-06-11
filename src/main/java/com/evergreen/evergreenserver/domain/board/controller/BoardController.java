59b0e8f0e3364a04fc549b32de1321156af24a08
k-jaehyun <wogus146@gmail.com>
2024-06-11 23:39:52 +0900

    fix: Board/post api 수정

--- src/main/java/com/evergreen/evergreenserver/domain/board/controller/BoardController.java
+++ src/main/java/com/evergreen/evergreenserver/domain/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,7 @@ public class BoardController {
 
   private final BoardService boardService;
 
-  @PostMapping("/post")
+  @PostMapping()
   public ResponseEntity<ApiResponse> postBoard(@RequestBody PostBoardDto postBoardDto,
       @AuthenticationPrincipal
       UserDetailsImpl userDetails) {
